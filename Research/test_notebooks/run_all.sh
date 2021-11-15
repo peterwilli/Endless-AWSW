0461fd902656f902ef334f3acd78b97629907a6a
Peter Willemsen <peter@codebuffet.co>
2021-11-15 11:14:21 +0100

    none of these tests worked >~<

--- Research/test_notebooks/run_all.sh
+++ Research/test_notebooks/run_all.sh
@@ -2,5 +2,5 @@
 for f in *.ipynb
 do
   echo "Processing $f..."
-  jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace "$f"
+  jupyter nbconvert --execute --allow-errors --to notebook --ExecutePreprocessor.timeout=-1 --inplace "$f"
 done
