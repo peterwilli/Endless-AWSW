ce9931c3e02e22bab56f38714458d04457b2eadd
Peter Willemsen <peter@codebuffet.co>
2021-11-29 14:29:45 +0100

    cleaned up code and added a test for commands + a scene command

--- DataExtractor/parser.py
+++ DataExtractor/parser.py
@@ -9,6 +9,7 @@ def extract_for_training(nodes):
         return None
     result = []
     buffer = []
+    last_scene = None
     last_speaker = None
     action_menu_regex = re.compile(r'\[\[.*?\]')
     character_images_regex = re.compile(r'{#.*?}(.*)')
@@ -58,33 +59,43 @@ def extract_for_training(nodes):
             allowed_lines = ["n", "m", "Rz", "Lo", "Ad", "c", "Ry", "Mv", "Br", "An", "Sb", "Wr", "Zh", "Kv", "Ka", "Em"]
             info = node.diff_info()
             if info[1] in allowed_lines:
-                prefix = ""
+                commands = []
                 if info[1] != last_speaker:
                     if info[1] == "c":
-                        prefix = "PlayerReply "
+                        prefix = "PlayerReply"
                     else:
-                        prefix = "DragonReply "
+                        prefix = "DragonReply"
+                    commands.append(prefix)
+                commands.append(info[1])                
                 should_end_buffer = False
                 if info[1] == "c" and last_speaker != None and last_speaker != "c":
                     should_end_buffer = True
                 if should_end_buffer:
                     safe_result_append(" ".join(buffer))
                     buffer = []
-                safe_buffer_append("{}{} \"{}\"".format(prefix, info[1], info[2]))
-                
+                commands.append("\"{}\"".format(info[2]))
+                if last_scene is not None:
+                    commands.append("scn {}".format(last_scene))
+                safe_buffer_append(" ".join(commands))
             last_speaker = info[1]
+        elif isinstance(node, renpy.ast.Scene):
+            last_scene = node.diff_info()[1][0]
         elif isinstance(node, renpy.ast.If):
             for entry in node.entries:
                 extracted_if_result = extract_for_training(entry[1])
                 safe_result_append(extracted_if_result)
         
     # add any excess replies to the last line...
-    return "\n".join(result) + " " + " ".join(buffer)
+    excess = ""
+    if len(result) > 0:
+        excess += " "
+    excess += " ".join(buffer)
+    return "\n".join(result) + excess
 
 def parse():
     script_folder = os.path.dirname(os.path.realpath(__file__))
     awsw_path = os.path.join(script_folder, "..", "Angels with Scaly Wings", "game")
-    #awsw_path = os.path.join(script_folder, "test_rpy")
+    # awsw_path = os.path.join(script_folder, "test_rpy")
     rpy_files = glob.glob(os.path.join(awsw_path, "*.rpy"))
     with open("training_data.txt", 'w') as training_data_fd:
         for rpy_file in rpy_files:
