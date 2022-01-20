f8564fa941f024dfed779c9e0f129524f88c476c
Peter Willemsen <peter@codebuffet.co>
2022-01-20 13:19:32 +0100

    validation buffer working?

--- EndlessServer/src/validated_reply_buffer.py
+++ EndlessServer/src/validated_reply_buffer.py
@@ -34,10 +34,13 @@ allowed_characters = {
     'Ad': 'Adine',
     'Sb': 'Sebastian'
 }
+allowed_scenes = ['park2', 'black', 'loremapt', 'office', 'bare', 'bareblur', 'bareblur2', 'pad', 'facin2', 'facinx', 'facin3', 'alley', 'farm', 'town4', 'beach', 'adineapt', 'corridor', 'emeraroom', 'o4', 'park3', 'np3x', 'np2x', 'np1x', 'buildingoutside', 'o2', 'np3', 'np2', 'store2', 'town1x', 'forestx', 'cave', 'o', 'remyapt', 'cafe', 'viewingspot', 'np1r', 'hallway', 'np2y', 'np1n', 'town2', 'stairs', 'darker', 'town1', 'store', 'library', 'school', 'forest1', 'forest2', 'storex', 'np5e', 'port1', 'beachx', 'padx', 'intro1', 'intro2', 'np4', 'np5', 'fac1', 'facin', 'town3', 'kitchen', 'np1', 'stars', 'o3', 'town7', 'town6', 'deadbody', 'whiteroom', 'office2', 'cave2', 'table', 'starsrx', 'hatchery', 'farm2', 'gate', 'testingroom', 'np6', 'fac12', 'adineapt2']        
+
 re_token = re.compile(r'(<.*?>|[^<]*)')
 re_command = re.compile(r'^<(.*?)>$')
 re_alphanumeric_whitespace = re.compile(r'[A-Za-z0-9\s]')
-re_within_message = re.compile(r'[\sa-zA-Z0-\[\]\-\+\?\"]')
+re_alphanumeric_scn = re.compile(r'[a-z0-9<>]')
+re_within_message = re.compile(r'[\sa-zA-Z0-\[\]\-\+\?\"\.]')
 
 class ValidationException(Exception):
     pass
@@ -50,18 +53,20 @@ class ValidatedReplyBuffer:
         self.expect_new_tokens(tokens_to_expect['cmd_p'])
         self.in_message = False
 
-    def expect_new_tokens(self, tokens):
+    def expect_new_tokens(self, tokens, index_override = 0):
         self.expect_tokens = tokens
-        self.expect_tokens_idx = 0
+        self.expect_tokens_idx = index_override
     
     def add_token(self, token):
+        if self.expect_tokens_idx >= len(self.expect_tokens):
+            raise Exception(f"expect_tokens_idx({self.expect_tokens_idx}) > expect_tokens {self.expect_tokens} ({len(self.expect_tokens)})")
         expected_token = self.expect_tokens[self.expect_tokens_idx]
         if type(expected_token) == re.Pattern:
             if expected_token.match(token) is None:
-                raise ValidationException(f"add_token[re]: {token} != {expected_token}!")
+                raise ValidationException(f"add_token[re]: expected '{expected_token}', got '{token}'!")
         else:
             if token != expected_token:
-                raise ValidationException(f"add_token[str]: {token} != {expected_token}!")
+                raise ValidationException(f"add_token[str]: expected '{expected_token}', got '{token}'!")
 
         self.tokens.append(token)
         self.expect_tokens_idx += 1
@@ -78,9 +83,21 @@ class ValidatedReplyBuffer:
                     if not self.last_cmd in allowed_commands:
                         raise ValidationException(f"add_token: {self.last_cmd} is not an allowed command!")
             if self.last_cmd == 'p':
+                self.last_side = 'p'
                 self.expect_new_tokens(tokens_to_expect['cmd_msg'])
             elif self.last_cmd == 'd':
                 self.last_side = 'd'
+                self.expect_new_tokens(tokens_to_expect['cmd_scn'])
+            elif self.last_cmd == 'scn':
+                if token == '<':
+                    # The scene ended
+                    scene = "".join(self.tokens[self.token_last_index(">") + 1:len(self.tokens) - 1])
+                    if not scene in allowed_scenes:
+                        raise ValidationException(f"add_token: scene '{scene}' is not in allowed_scenes!")
+                    # We already have the <
+                    self.expect_new_tokens(tokens_to_expect['cmd_msg'], index_override = 1)
+                else:
+                    self.expect_new_tokens([re_alphanumeric_scn])
             elif self.last_cmd == 'msg':
                 if token == ' ':
                     # with a space we check the character that came before
@@ -98,8 +115,7 @@ class ValidatedReplyBuffer:
                         elif self.last_side == 'd':
                             self.expect_new_tokens(tokens_to_expect['cmd_p'])
                         else:
-                            # TODO: fix the side issue
-                            raise Exception(f"invalid last side: {self.last_side} can either be d or p! (side given: {token})")
+                            raise Exception(f"invalid last side: {self.last_side} can either be d or p!")
                 elif self.in_message:
                     self.expect_new_tokens([re_within_message])
                 else:
@@ -113,9 +129,19 @@ class ValidatedReplyBuffer:
 
 if __name__ == '__main__':
     def test_tokens(tokens):
+        print(f"testing: {tokens}")
         buffer = ValidatedReplyBuffer()
         for t in tokens:
             buffer.add_token(t)
         assert buffer.squeeze() == tokens
 
     test_tokens('<p><msg>c "Flooding?"<d><scn>o2<msg>Sb "Yes."')
+    try:
+        test_tokens('<p><msgf>c "Flooding?"<d><scn>o2<msg>Sb "Yes."')
+    except ValidationException as e:
+        print(e)
+    try:
+        test_tokens('<p><msg>c "Floodi"ng?"<d><scn>o2<msg>Sb "Yes."')
+    except ValidationException as e:
+        print(e)
+    
