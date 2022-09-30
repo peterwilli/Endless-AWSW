227bb3c108c4e9aae8fced5025c8105568691518
Peter Willemsen <peter@codebuffet.co>
2022-10-01 01:18:06 +0200

    second try of flashcard based training with multiple sentences going back (max 4 at this time of writing). For some reason it bugs out and I have to restart training (same error as previous commit). I dont think its my code

--- Research/model_utils.py
+++ Research/model_utils.py
@@ -189,6 +189,7 @@ def get_dataset(seed, tokenizer, path_train, block_size = 128):
         return { 'text': result }
     
     def filter_per_character(batch):
+        result = []
         from_moments = {}
         for character in Config.interactable_characters:
             from_moments[character] = []
@@ -201,24 +202,24 @@ def get_dataset(seed, tokenizer, path_train, block_size = 128):
                     raise Exception(f"msg_match None! Line: '{line}'")
                 msg_from = msg_match.group(1)
                 if msg_from == character:
-                    from_moments[msg_from].append(idx)
-                    
-                    
-        result_texts = []             
+                    from_moments[msg_from].append(idx) 
+
         while True:
             for character in Config.interactable_characters:
                 if len(from_moments[character]) > 0:
                     text_to_add = ""
                     moment = random.choice(from_moments[character])
                     if moment > 0:
-                        before_slice = min(random.randint(1, 4), moment)
-                        text_before = "".join(batch['text'][moment - before_slice:moment - 1])
-                        text_to_add += text_before
+                        before_slice = random.randint(1, min(moment, 4))
+                        start = moment - before_slice
+                        text_before = "".join(batch['text'][start:moment])
+                        if len(text_before) > 0:
+                            text_to_add += text_before
                     text_to_add += batch['text'][moment]
                     from_moments[character].remove(moment)
-                    result_texts.append(text_to_add)
+                    result.append(text_to_add)
                     if len(from_moments[character]) == 0:
-                        return { 'text': result_texts }
+                        return { 'text': result }
 
     def group_texts(examples):
         # Concatenate all texts.
